4db745be16a60371c88d0b9181ee7bdf6bd771cf
Sandy Cormie <sandy.cormie@linn.co.uk>
2013-09-04 09:24:13 +0100

    Removed servicestack

--- src/Kola/Nancy/KolaNancyBootstrapper.cs
+++ src/Kola/Nancy/KolaNancyBootstrapper.cs
@@ -1,24 +1,46 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using Kola.Configuration.Ideas;
-using Nancy;
-using Nancy.Bootstrapper;
-using Nancy.Conventions;
-using Nancy.Diagnostics;
-using Nancy.TinyIoc;
-using Nancy.ViewEngines;
-using Nancy.ViewEngines.Razor;
-using Nancy.Embedded.Conventions;
-
-namespace Kola.Nancy
+﻿namespace Kola.Nancy
 {
-    using global::Nancy.Serializers.Json.ServiceStack;
+    using System;
+    using System.Linq;
+    using System.Reflection;
 
-    using ServiceStack.Text;
+    using Kola.Configuration.Ideas;
+
+    using global::Nancy;
+    using global::Nancy.Bootstrapper;
+    using global::Nancy.Conventions;
+    using global::Nancy.Embedded.Conventions;
+    using global::Nancy.TinyIoc;
+    using global::Nancy.ViewEngines;
+    using global::Nancy.ViewEngines.Razor;
+
+    //using global::Nancy.Serializers.Json.ServiceStack;
+
+    //using ServiceStack.Text;
 
     public class KolaNancyBootstrapper : DefaultNancyBootstrapper
     {
+
+        // TODO : A better way of adding plugins to AppDomainAssemblyTypeScanner is required :)
+        public static Func<Assembly, bool>[] CandidatePluginAssemblies = new Func<Assembly, bool>[]
+        {
+            x => x.GetReferencedAssemblies().Any(r => r.Name.StartsWith("Kola"))
+        };
+
+        protected override NancyInternalConfiguration InternalConfiguration
+        {
+            get
+            {
+                return NancyInternalConfiguration.WithOverrides(c =>
+                {
+                    var i = c.Serializers.Count();
+                    //c.Serializers.Insert(0, typeof(global::Nancy.Serializers.Json.ServiceStack.ServiceStackJsonSerializer));
+                    //if (c.Serializers.Count() == 0 || c.Serializers.OfType<ServiceStackJsonSerializer>().Count() == 0)
+                    //    c.Serializers.Insert(0, typeof(ServiceStackJsonSerializer));
+                    c.ViewLocationProvider = typeof(ResourceViewLocationProvider);
+                });
+            }
+        }
 
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
         {
@@ -37,37 +59,17 @@ namespace Kola.Nancy
             base.ConfigureApplicationContainer(container);
         }
 
-        // TODO : A better way of adding plugins to AppDomainAssemblyTypeScanner is required :)
-        public static Func<Assembly, bool>[] CandidatePluginAssemblies = new Func<Assembly, bool>[]
-        {
-            x => x.GetReferencedAssemblies().Any(r => r.Name.StartsWith("Kola"))
-        };
-
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
 
             //Elmahlogging.Enable(pipelines, "elmah");
 
-            JsConfig.DateHandler = JsonDateHandler.ISO8601;
-            JsConfig.EmitCamelCaseNames = true;
-            JsConfig.ExcludeTypeInfo = true;
-            JsConfig<Guid>.SerializeFn = g => g.ToString(); // otherwise it excludes hypens 
-            JsConfig<Guid?>.SerializeFn = g => g.Value.ToString(); // otherwise it excludes hypens
-        }
-
-        protected override NancyInternalConfiguration InternalConfiguration
-        {
-            get
-            {
-                return NancyInternalConfiguration.WithOverrides(c =>
-                    {
-                        c.Serializers.Insert(0, typeof(global::Nancy.Serializers.Json.ServiceStack.ServiceStackJsonSerializer));
-                        //if (c.Serializers.Count() == 0 || c.Serializers.OfType<ServiceStackJsonSerializer>().Count() == 0)
-                        //    c.Serializers.Insert(0, typeof(ServiceStackJsonSerializer));
-                        c.ViewLocationProvider = typeof(ResourceViewLocationProvider);
-                    });
-            }
+            //JsConfig.DateHandler = JsonDateHandler.ISO8601;
+            //JsConfig.EmitCamelCaseNames = true;
+            //JsConfig.ExcludeTypeInfo = true;
+            //JsConfig<Guid>.SerializeFn = g => g.ToString(); // otherwise it excludes hypens 
+            //JsConfig<Guid?>.SerializeFn = g => g.Value.ToString(); // otherwise it excludes hypens
         }
 
         protected override void ConfigureConventions(NancyConventions conventions)
@@ -77,19 +79,5 @@ namespace Kola.Nancy
             conventions.StaticContentsConventions.Add(EmbeddedStaticContentConventionBuilder.AddDirectory("/_kola/Scripts", typeof(KolaNancyBootstrapper).Assembly, "/Scripts"));
             conventions.StaticContentsConventions.Add(EmbeddedStaticContentConventionBuilder.AddDirectory("/_kola/Content", typeof(KolaNancyBootstrapper).Assembly, "/Content"));
         }
-
-        #region diagnostics
-
-        //protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
-        //{
-        //    StaticConfiguration.EnableRequestTracing = true;
-        //}
-
-        //protected override DiagnosticsConfiguration DiagnosticsConfiguration
-        //{
-        //    get { return new DiagnosticsConfiguration { Password = @"123" }; }
-        //}
-
-        #endregion
     }
 }
