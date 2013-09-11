664623956b7edb464a3c8736adc5165c4eda8636
Sandy Cormie <sandy.cormie@linn.co.uk>
2013-09-12 00:08:21 +0100

    Thinking about backbone model

---
+++ src/Kola/Scripts/app/models/ComponentContainer.js
@@ -0,0 +1,30 @@
+﻿define([
+    'backbone',
+    'underscore',
+    'app/collections/Components'
+], function (Backbone,
+    _,
+    Components) {
+
+    "use strict";
+
+    return Backbone.Model.extend({
+
+        initialize: function (url) {
+            this.components = new Components();
+            this.url = url;
+        },
+
+        addComponent: function (component) {
+            alert("Adding Component");
+            //this.components.add(component);
+        },
+
+        parse: function (resp, options) {
+
+            var id = _.find(resp.links, function (l) { return l.rel == "self" }).href;
+
+            return _.extend(resp, { id: id });
+        }
+    });
+});
