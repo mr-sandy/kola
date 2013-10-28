fdb664486245d72c4e13a235aa9ecad88102c643
Sandy Cormie <sandy.cormie@linn.co.uk>
2013-10-28 22:49:23 +0000

    Tweaking setting of URL on amendment parse

--- src/Kola/Scripts/app/models/Amendment.js
+++ src/Kola/Scripts/app/models/Amendment.js
@@ -1,8 +1,11 @@
-﻿define(function () {
+﻿define([    'app/Config',
+], function (Config) {
     'use strict';
 
     return {
         parse: function (resp, options) {
+            this.url = this.getLink(resp.links, 'self');
+            this.url = this.combineUrls(Config.kolaRoot, this.getLink(resp.links, 'self'));
             return _.extend(resp,
             {
                 url: this.getLink(resp.links, 'self'),
