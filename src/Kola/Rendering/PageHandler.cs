b9d0fe92a7cbd30fd1d172dcb4574ece8ccb04d8
Sandy Cormie <sandy.cormie@linn.co.uk>
2014-01-08 00:35:33 +0000

    Added page handler

---
+++ src/Kola/Rendering/PageHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Kola.Rendering
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Kola.Editing;
+    using Kola.Persistence;
+
+    public class PageHandler : IPageHandler
+    {
+        private readonly ITemplateRepository templateRepository;
+
+        public PageHandler(ITemplateRepository templateRepository)
+        {
+            this.templateRepository = templateRepository;
+        }
+
+        public IPage GetPage(IEnumerable<string> path)
+        {
+            var template = this.templateRepository.Get(path);
+
+            if (template == null)
+            {
+                return null;
+            }
+
+            return this.MakePage(template);
+        }
+
+        private IPage MakePage(Template template)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
