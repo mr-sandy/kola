4fd1cc4236519b4c7fd8c7185b8610f450becbf5
Sandy Cormie <sandy.cormie@linn.co.uk>
2014-06-25 11:55:31 +0100

    Added tests for building template resources

--- src/Kola/Extensions/TemplateResourceBuilder.cs
+++ src/Kola/Extensions/TemplateResourceBuilder.cs
@@ -11,79 +11,14 @@
     {
         public TemplateResource Build(Template template)
         {
-            var vistor = new ResourceBuildingComponentVisitor2();
+            //var visitor = new ComponentResourceBuildingVisitor();
 
-            var components = new List<ComponentResource>();
+            //var components = visitor.Build(template.Components);
 
-            for (var i = 0; i < template.Components.Count(); i++)
-            {
-                var component = template.Components.ElementAt(i);
-
-               // components.Add(component.Accept(vistor, Enumerable.Empty<int>()));
-            }
-
-            return new TemplateResource { Components = components };
-        }
-    }
-
-    internal class ResourceBuildingComponentVisitor2 : IComponentVisitor2<ComponentResource, IEnumerable<int>>
-    {
-        public ComponentResource Visit(Atom atom, IEnumerable<int> context)
-        {
-            return new ComponentResource
+            return new TemplateResource
                 {
-                    Name = atom.Name
                 };
         }
-
-        public ComponentResource Visit(Container container, IEnumerable<int> context)
-        {
-            return new ComponentResource
-            {
-                Name = container.Name,
-                Components = this.BuildChildResources(container, context)
-            };
-        }
-
-        public ComponentResource Visit(Widget widget, IEnumerable<int> context)
-        {
-            return new ComponentResource
-            {
-                Name = widget.Name,
-            };
-        }
-
-        public ComponentResource Visit(Placeholder placeholder, IEnumerable<int> context)
-        {
-            return new ComponentResource
-            {
-                Name = "placeholder"
-            };
-        }
-
-        private IEnumerable<ComponentResource> BuildChildResources(IComponentCollection componentCollection, IEnumerable<int> context)
-        {
-            var components = new List<ComponentResource>();
-
-            for (var i = 0; i < componentCollection.Components.Count(); i++)
-            {
-                var component = componentCollection.Components.ElementAt(i);
-
-                //components.Add(component.Accept(this, context.Append(i)));
-            }
-
-            return components;
-        }
     }
 
-    public interface IComponentVisitor2<out TResult, in TContext>
-    {
-        TResult Visit(Atom atom, TContext context);
-
-        TResult Visit(Container container, TContext context);
-
-        TResult Visit(Widget widget, TContext context);
-
-        TResult Visit(Placeholder placeholder, TContext context);
-    }
 }
---
+++ src/Tests/Unit.Tests/ResourceBuilding/WhenRenderingAPage.cs
@@ -0,0 +1,56 @@
+﻿namespace Unit.Tests.ResourceBuilding
+{
+    using System.Linq;
+
+    using FluentAssertions;
+
+    using Kola.Domain.Composition;
+    using Kola.Extensions;
+    using Kola.Resources;
+
+    using NUnit.Framework;
+
+    public class WhenBuildingATemplateResource
+    {
+        private TemplateResource resource;
+
+        [SetUp]
+        public void EstablishContext()
+        {
+            var template = new Template(new[] { "test", "path" }, new IComponent[]
+                    {
+                        new Atom("atom 0"), 
+                        new Container("container 1", components: new[] { new Atom("atom 1.0") }),
+                        new Widget("widget 2", areas: new[] { new Area(new[] { new Atom("atom 2.0.0") }), })
+                    });
+
+            var builder = new TemplateResourceBuilder();
+
+            this.resource = builder.Build(template);
+        }
+
+        [Test]
+        public void ShouldHaveFourRootComponents()
+        {
+            this.resource.Components.Should().HaveCount(4);
+        }
+
+        [Test]
+        public void ShouldSetCorrectNameForComponent0()
+        {
+            this.resource.Components.ElementAt(0).Name.Should().Be("atom 0");
+        }
+
+        [Test]
+        public void ShouldSetCorrectNameForComponent1()
+        {
+            this.resource.Components.ElementAt(1).Name.Should().Be("container 1");
+        }
+
+        [Test]
+        public void ShouldSetCorrectNameForComponent2()
+        {
+            this.resource.Components.ElementAt(2).Name.Should().Be("widget 2");
+        }
+    }
+}
