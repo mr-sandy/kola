3e7188dbada607f801d5291cb8f875e0992f8b00
Sandy Cormie <sandy.cormie@linn.co.uk>
2014-06-30 00:07:07 +0100

    Started with new vistor for finding components

---
+++ src/Kola/Domain/Composition/CollectionFindingComponentVisitor.cs
@@ -0,0 +1,28 @@
+﻿namespace Kola.Domain.Composition
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class CollectionFindingComponentVisitor : IComponentVisitor<IComponent, IEnumerable<int>>
+    {
+        public IComponent Visit(Atom atom, IEnumerable<int> context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent Visit(Container container, IEnumerable<int> context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent Visit(Widget widget, IEnumerable<int> context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IComponent Visit(Placeholder placeholder, IEnumerable<int> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
