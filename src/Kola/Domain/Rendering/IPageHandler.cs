c22c598d941204cd5c4c96c216bee166829a828b
Sandy Cormie <sandy.cormie@linn.co.uk>
2014-07-01 21:39:37 +0100

    Added preview module

--- src/Kola/Domain/Rendering/IPageHandler.cs
+++ src/Kola/Domain/Rendering/IPageHandler.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
 
-    using Kola.Domain;
     using Kola.Domain.Instances;
 
     public interface IPageHandler
---
+++ src/Kola/Nancy/Modules/PreviewModule.cs
@@ -0,0 +1,32 @@
+﻿namespace Kola.Nancy.Modules
+{
+    using Kola.Domain.Rendering;
+    using Kola.Extensions;
+
+    using global::Nancy;
+    using global::Nancy.Responses.Negotiation;
+
+    public class PreviewModule : NancyModule
+    {
+        private readonly IPageHandler pageHandler;
+
+        public PreviewModule(IPageHandler pageHandler)
+        {
+            this.pageHandler = pageHandler;
+            this.Get["/_kola/preview/(?<rawPath>.*)", AcceptHeaderFilters.Html] = p => this.GetPage(p.rawPath);
+            this.Get["/_kola/preview/", AcceptHeaderFilters.Html] = p => this.GetPage(string.Empty);
+        }
+
+        // TODO {SC} This should renamed GetContent; the pageHandler should be a content handler, 
+        // and should return different types of content: pages; redirects; and 404 content?
+        private Negotiator GetPage(string rawPath)
+        {
+            var path = rawPath.ParsePath();
+            var page = this.pageHandler.GetPage(path);
+
+            return this.Negotiate
+                .WithModel(page)
+                .WithView("Page");
+        }
+    }
+}
