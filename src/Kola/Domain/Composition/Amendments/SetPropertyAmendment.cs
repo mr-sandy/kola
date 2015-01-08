98953981e07ae77c498c1dcc0b0b0d8f6abf8d1b
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-01-08 17:00:41 +0000

    Added  missed files

---
+++ src/Kola/Domain/Composition/Amendments/SetPropertyAmendment.cs
@@ -0,0 +1,43 @@
+﻿namespace Kola.Domain.Composition.Amendments
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Kola.Extensions;
+
+    public class SetPropertyAmendment : IAmendment
+    {
+        public SetPropertyAmendment(IEnumerable<int> componentPath, string propertyName, string value)
+        {
+            this.ComponentPath = componentPath;
+            this.PropertyName = propertyName;
+            this.Value = value;
+        }
+
+        public IEnumerable<int> ComponentPath { get; private set; }
+
+        public string PropertyName { get; private set; }
+
+        public string Value { get; private set; }
+
+        public IEnumerable<IEnumerable<int>> SubjectPaths
+        {
+            get { yield return this.ComponentPath; }
+        }
+
+        public void Accept(IAmendmentVisitor visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Accept<T>(IAmendmentVisitor<T> visitor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Accept<T, TContext>(IAmendmentVisitor<T, TContext> visitor, TContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Kola/Resources/SetPropertyAmendmentResource.cs
@@ -0,0 +1,21 @@
+﻿namespace Kola.Resources
+{
+    public class SetPropertyAmendmentResource : AmendmentResource
+    {
+        public string ComponentPath { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public string Value { get; set; }
+
+        public override string Type
+        {
+            get { return "Set Property"; }
+        }
+
+        public override T Accept<T>(IAmendmentResourceVisitor<T> visitor)
+        {
+            return visitor.Visit(this);
+        }
+    }
+}
