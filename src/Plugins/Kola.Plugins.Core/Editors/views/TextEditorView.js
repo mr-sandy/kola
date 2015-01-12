80b5378fecb2d19d4107bd1f952ead588a38dc74
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-01-12 09:38:02 +0000

    Added auto selction to text editor view

--- src/Plugins/Kola.Plugins.Core/Editors/views/TextEditorView.js
+++ src/Plugins/Kola.Plugins.Core/Editors/views/TextEditorView.js
@@ -12,6 +12,10 @@
         render: function (editMode) {
             if (editMode) {
                 this.$el.html(this.template(this.model));
+                var input = this.$el.find('input');
+                input.focus();
+                input[0].setSelectionRange(0, input.val().length);
+                //input[0].setSelectionRange(0, 9)
             }
             else {
                 this.$el.html(this.model.value.value);
