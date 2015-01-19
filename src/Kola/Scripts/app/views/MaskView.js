85f7c5279b6d684f47fa2f15330e1595f0867d9e
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-01-19 10:48:33 +0000

    Fixed style on mask view

--- src/Kola/Scripts/app/views/MaskView.js
+++ src/Kola/Scripts/app/views/MaskView.js
@@ -22,7 +22,8 @@
                 'top': 0,
                 'bottom': 0,
                 'left': 0,
-                'right': 0
+                'right': 0,
+                'pointer-events': 'none'
             });
 
             this.topMask = this.$('#topMask');
