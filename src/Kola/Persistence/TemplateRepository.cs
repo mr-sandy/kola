433f4b08c4c8ac1d25ea1b1a946eab42ba7efb64
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-02-16 13:37:02 +0000

    Parameterised template file location

--- src/Kola/Persistence/TemplateRepository.cs
+++ src/Kola/Persistence/TemplateRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace Kola.Persistence
 {
     using System.Collections.Generic;
+    using System.Configuration;
 
     using Kola.Domain.Composition;
     using Kola.Extensions;
@@ -10,22 +11,29 @@
 
     internal class TemplateRepository : ITemplateRepository
     {
-        private const string RootDirectory = @"C:\projects\kola\src\Kola\Persistence\Templates";
+        //private const string RootDirectory = @"C:\projects\kola\src\Kola\Persistence\Templates";
         private const string TemplateFileName = "Template.xml";
 
         private readonly SerializationHelper serializationHelper;
         private readonly FileSystemHelper fileSystemHelper;
+        private readonly string rootDirectory;
 
         public TemplateRepository(SerializationHelper serializationHelper, FileSystemHelper fileSystemHelper)
+            : this(serializationHelper, fileSystemHelper, rootDirectory: ConfigurationManager.AppSettings["ContentRoot"])
+        {
+        }
+
+        public TemplateRepository(SerializationHelper serializationHelper, FileSystemHelper fileSystemHelper, string rootDirectory)
         {
             this.serializationHelper = serializationHelper;
             this.fileSystemHelper = fileSystemHelper;
+            this.rootDirectory = rootDirectory;
         }
 
         public void Add(Template template)
         {
             var surrogate = new TemplateSurrogateBuilder().Build(template);
-            var directoryPath = this.fileSystemHelper.CombinePaths(RootDirectory, template.Path.ToFileSystemPath());
+            var directoryPath = this.fileSystemHelper.CombinePaths(this.rootDirectory, template.Path.ToFileSystemPath());
 
             if (!this.fileSystemHelper.DirectoryExists(directoryPath))
             {
@@ -38,7 +46,7 @@
 
         public Template Get(IEnumerable<string> templatePath)
         {
-            var path = this.fileSystemHelper.CombinePaths(RootDirectory, templatePath.ToFileSystemPath(), TemplateFileName);
+            var path = this.fileSystemHelper.CombinePaths(this.rootDirectory, templatePath.ToFileSystemPath(), TemplateFileName);
 
             if (!this.fileSystemHelper.FileExists(path))
             {
@@ -52,7 +60,7 @@
         public void Update(Template template)
         {
             var surrogate = new TemplateSurrogateBuilder().Build(template);
-            var path = this.fileSystemHelper.CombinePaths(RootDirectory, template.Path.ToFileSystemPath(), TemplateFileName);
+            var path = this.fileSystemHelper.CombinePaths(this.rootDirectory, template.Path.ToFileSystemPath(), TemplateFileName);
             this.serializationHelper.Serialize<TemplateSurrogate>(surrogate, path);
         }
     }
--- src/Kola/nuget/nuget-kola.ps1
+++ src/Kola/nuget/nuget-kola.ps1
@@ -1,3 +1,4 @@
+.\NuGet.exe spec -f -AssemblyPath ..\bin\Debug\Kola.dll
 .\NuGet.exe pack Kola.nuspec
 .\NuGet.exe setApiKey C33FB0EC-F395-4EDC-87ED-48F644AC5457 -Source http://code.it.linn.co.uk
 .\NuGet.exe push .\Kola.*.nupkg C33FB0EC-F395-4EDC-87ED-48F644AC5457 -s http://code.it.linn.co.uk
