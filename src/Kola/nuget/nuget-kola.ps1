1200fbc8df126ef5acb324244450f9290de0f800
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-02-16 12:07:58 +0000

    Added nuget scripts

---
+++ src/Kola/nuget/nuget-kola.ps1
@@ -0,0 +1,3 @@
+.\NuGet.exe pack Kola.nuspec
+.\NuGet.exe setApiKey C33FB0EC-F395-4EDC-87ED-48F644AC5457 -Source http://code.it.linn.co.uk
+.\NuGet.exe push .\Kola.*.nupkg C33FB0EC-F395-4EDC-87ED-48F644AC5457 -s http://code.it.linn.co.uk
