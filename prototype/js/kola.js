371c3fdd1af973ff497d7532031281ca985583f0
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-03-19 00:05:31 +0000

    Added new prototype for edutor styles

---
+++ prototype/js/kola.js
@@ -0,0 +1,20 @@
+﻿(function ($) {
+    $(document).ready(function () {
+        $('#hide-sidebar').click(function (e) {
+            $('#sidebar').addClass('hidden');
+            $('#sidebar-proxy').removeClass('hidden');
+            $('#preview').addClass('fullscreen');
+        });
+
+        $('#show-sidebar').click(function (e) {
+            $('#sidebar').removeClass('hidden');
+            $('#sidebar-proxy').addClass('hidden');
+            $('#preview').removeClass('fullscreen');
+
+        });
+
+        $('#toggle-toolbars').click(function (e) {
+            $('#toolbars').toggleClass('hidden');
+        });
+    });
+})(jQuery);
