35a5672a4780b03230d8d91fa55dcbb675b760e9
Sandy Cormie <sandy.cormie@linn.co.uk>
2015-11-12 16:15:33 +0000

    Updated Nancy packahes to latest versions; Update Sample.Host to use OWIN

---
+++ src/Samples/Sample.Host/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+
+using Sample.Host;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Sample.Host
+{
+    using Microsoft.Owin.Extensions;
+
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
+    }
+}
