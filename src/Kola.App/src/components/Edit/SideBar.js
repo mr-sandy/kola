6dd20b85e3648a2ef5a879b29ec76d059e0d4cdd
Sandy Cormie <sandy.cormie@linn.co.uk>
2017-02-01 21:15:33 +0000

    Added font-awesome

--- src/Kola.App/src/components/Edit/SideBar.js
+++ src/Kola.App/src/components/Edit/SideBar.js
@@ -11,10 +11,34 @@ const style = {
     paddingTop: '4px',
     overflow: 'hidden'};
 
+const footerStyle = {
+    position: 'absolute',
+    bottom: '0',
+    left: '0',
+    right: '0',
+    padding: '10px'
+};
+
+const buttonStyle =
+{
+    height: '36px',
+    width: '36px',
+    color: '#fff',
+    backgroundColor: '#555',
+    borderWidth: '2px',
+    borderColor: '#555',
+    borderStyle: 'solid'
+}
+
 const SideBar = props => {
 
-    return (<div style={style}>
-        
+    return (
+    <div style={style}>
+        <div style={footerStyle}>
+            <button style={buttonStyle} type="button" title="Pin Toolbars">
+                <i className="fa fa-thumb-tack fa-lg"></i>
+            </button>
+        </div>
     </div>
     );
 }
