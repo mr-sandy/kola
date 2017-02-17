8307ffdfd15f51ea0590c90e57ba28b9bf78498f
Sandy Cormie <sandy.cormie@linn.co.uk>
2017-02-17 09:59:26 +0000

    Made an area adrop target to handle hovering over area name label

--- src/Kola.App/src/components/Edit/Structure/Area.js
+++ src/Kola.App/src/components/Edit/Structure/Area.js
@@ -1,16 +1,38 @@
 ﻿import React  from 'react';
+import { DropTarget } from 'react-dnd';
 import ComponentList from './ComponentList';
+import { toIntArray } from '../../../utility';
 
 const captionStyle = {
     display: 'block',
     padding: '4px 0 8px 0'
 };
 
-const Area = ({ area, ...props }) => (
+const dropTarget = {
+    drop(props, monitor) {
+    },
+
+    hover(props, monitor) {
+        if (monitor.isOver({ shallow: true })) {
+            const { showPlaceholder, area} = props;
+            if (area.components.length === 0) {
+                showPlaceholder('/' + [...toIntArray(area.path), 0].join('/'));
+            }
+        }
+    }
+};
+
+function dropCollect(connect) {
+    return {
+        connectDropTarget: connect.dropTarget()
+    };
+}
+
+const Area = ({ area, connectDropTarget, ...props }) => connectDropTarget(
     <div>
         <span style={captionStyle}>{`area: ${area.name}`}</span>
         <ComponentList components={area.components} componentPath={area.path} {...props} />
     </div>
 );
 
-export default Area;
+export default DropTarget(['COMPONENT', 'COMPONENT_TYPE'], dropTarget, dropCollect)(Area);
